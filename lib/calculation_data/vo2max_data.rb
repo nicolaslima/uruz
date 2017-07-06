5e22866f1cc7c1a1e4cc367d89bbbdff5fe9a6b3
Nicolas Lima <lima.nicolasmateus@gmail.com>
2017-07-06 00:10:43 -0400

    fix infinit convertion

--- lib/calculation_data/vo2max_data.rb
+++ lib/calculation_data/vo2max_data.rb
@@ -1,21 +1,21 @@
 module Uruz
 
 	$vo2max_male_data = [
-		{:age => "13..19",      :very_weak => "-35..-9999",   :weak => "35.1..38.3", :middle => "38.4..45.1", :good => "45.2..50.9", :excellent => "51.0..55.9", :higher => "56.0..9999", :gender => "male"},
-		{:age => "20..29",      :very_weak => "-33..-9999",   :weak => "33.1..36.4", :middle => "36.5..42.4", :good => "42.5..46.4", :excellent => "46.5..52.4", :higher => "52.5..9999", :gender => "male"},
-		{:age => "30..39",      :very_weak => "-31.5..-9999", :weak => "31.6..35.4", :middle => "35.5..40.9", :good => "41.0..44.9", :excellent => "45.0..49.4", :higher => "49.5..9999", :gender => "male"},
-		{:age => "40..49",      :very_weak => "-30.2..-9999", :weak => "30.3..33.5", :middle => "33.6..38.9", :good => "39.0..43.7", :excellent => "43.8..48.0", :higher => "48.1..9999", :gender => "male"},
-		{:age => "50..59",      :very_weak => "-26.1..-9999", :weak => "26.2..30.9", :middle => "31.0..35.7", :good => "35.8..40.9", :excellent => "41.0..45.3", :higher => "45.4..9999", :gender => "male"},
-		{:age => "60..9999",    :very_weak => "-20.5..-9999", :weak => "20.6..26.0", :middle => "26.1..32.3", :good => "32.3..36.4", :excellent => "36.5..44.2", :higher => "44.3..9999", :gender => "male"}
+		{:age => "13..19",      :very_weak => "-9999..35",   :weak => "35.1..38.3", :middle => "38.4..45.1", :good => "45.2..50.9", :excellent => "51.0..55.9", :higher => "56.0..9999", :gender => "male"},
+		{:age => "20..29",      :very_weak => "-9999..33",   :weak => "33.1..36.4", :middle => "36.5..42.4", :good => "42.5..46.4", :excellent => "46.5..52.4", :higher => "52.5..9999", :gender => "male"},
+		{:age => "30..39",      :very_weak => "-9999..31.5", :weak => "31.6..35.4", :middle => "35.5..40.9", :good => "41.0..44.9", :excellent => "45.0..49.4", :higher => "49.5..9999", :gender => "male"},
+		{:age => "40..49",      :very_weak => "-9999..30.2", :weak => "30.3..33.5", :middle => "33.6..38.9", :good => "39.0..43.7", :excellent => "43.8..48.0", :higher => "48.1..9999", :gender => "male"},
+		{:age => "50..59",      :very_weak => "-9999..26.1", :weak => "26.2..30.9", :middle => "31.0..35.7", :good => "35.8..40.9", :excellent => "41.0..45.3", :higher => "45.4..9999", :gender => "male"},
+		{:age => "60..9999",    :very_weak => "-9999..20.5", :weak => "20.6..26.0", :middle => "26.1..32.3", :good => "32.3..36.4", :excellent => "36.5..44.2", :higher => "44.3..9999", :gender => "male"}
 	]
 
 	$vo2max_female_data = [
-		{:age => "13..19",      :very_weak => "-25..-9999", 	:weak => "25.1..39.9", :middle => "31.0..34.9", :good => "35.0..38.9", :excellent => "39.0..41.9", :higher => "42.0..9999",  :gender  => "female"},
-		{:age => "20..29",      :very_weak => "-23.6..-9999", :weak => "23.7..28.9", :middle => "29.0..32.9", :good => "33.0..36.9", :excellent => "37.0..40.9", :higher => "41.0..9999",  :gender  => "female"},
-		{:age => "30..39",      :very_weak => "-22.8..-9999", :weak => "22.9..26.9", :middle => "27.0..31.4", :good => "31.5..35.6", :excellent => "35.7..40.0", :higher => "40.1..9999",  :gender  => "female"},
-		{:age => "40..49",      :very_weak => "-21..-9999", 	:weak => "21.1..24.4", :middle => "24.5..28.9", :good => "29.0..32.8", :excellent => "32.9..36.9", :higher => "37.0..9999",  :gender  => "female"},
-		{:age => "50..59",      :very_weak => "-20.2..-9999", :weak => "20.3..22.7", :middle => "22.8..26.9", :good => "27.0..31.4", :excellent => "31.5..35.7", :higher => "35.8..9999",  :gender  => "female"},
-		{:age => "60..9999",    :very_weak => "-17.5..-9999", :weak => "17.6..20.1", :middle => "20.2..24.4", :good => "24.5..30.2", :excellent => "30.3..31.4", :higher => "31.50..9999", :gender => "female"}
+		{:age => "13..19",      :very_weak => "-9999..25", 	 :weak => "25.1..39.9", :middle => "31.0..34.9", :good => "35.0..38.9", :excellent => "39.0..41.9", :higher => "42.0..9999",  :gender  => "female"},
+		{:age => "20..29",      :very_weak => "-9999..23.6", :weak => "23.7..28.9", :middle => "29.0..32.9", :good => "33.0..36.9", :excellent => "37.0..40.9", :higher => "41.0..9999",  :gender  => "female"},
+		{:age => "30..39",      :very_weak => "-9999..22.8", :weak => "22.9..26.9", :middle => "27.0..31.4", :good => "31.5..35.6", :excellent => "35.7..40.0", :higher => "40.1..9999",  :gender  => "female"},
+		{:age => "40..49",      :very_weak => "-9999..21", 	 :weak => "21.1..24.4", :middle => "24.5..28.9", :good => "29.0..32.8", :excellent => "32.9..36.9", :higher => "37.0..9999",  :gender  => "female"},
+		{:age => "50..59",      :very_weak => "-9999..20.2", :weak => "20.3..22.7", :middle => "22.8..26.9", :good => "27.0..31.4", :excellent => "31.5..35.7", :higher => "35.8..9999",  :gender  => "female"},
+		{:age => "60..9999",    :very_weak => "-9999..17.5", :weak => "17.6..20.1", :middle => "20.2..24.4", :good => "24.5..30.2", :excellent => "30.3..31.4", :higher => "31.50..9999", :gender  => "female"}
 	]
 
 end
